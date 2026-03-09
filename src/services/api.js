7c41b8b9b80e86b8c14c82054e5d3dfddcbca7a9
DavidSanchez287761 <287761@student@pwr.edu.pl>
2026-03-09 19:55:25 +0100

    Remove node_modules from repository

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://mimesa-backend.onrender.com' });
+const api = axios.create({ baseURL: 'https://mimesa-backend.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
